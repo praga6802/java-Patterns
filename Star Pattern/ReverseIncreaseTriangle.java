22f53ef9a8de42324cc796406a146f1a1f6c6ec7
praga6802 <praga.06.08@gmail.com>
2025-03-19 10:29:22 +0530

    added star programs

---
+++ Star Pattern/ReverseIncreaseTriangle.java
@@ -0,0 +1,13 @@
+public class ReverseIncreaseTriangle{
+	public static void main(String[] args){
+		for(int row=1;row<=5;row++){	
+		for(int col=row;col<5;col++){
+			System.out.print(" ");
+		}
+		for(int col=1;col<=row;col++){
+			System.out.print("*");
+		}
+	System.out.println();
+	}
+}
+}
