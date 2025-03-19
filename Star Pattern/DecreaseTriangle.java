470e2688a35819a703b7eb2a9e56bc0643d0172d
praga6802 <praga.06.08@gmail.com>
2025-03-19 10:16:29 +0530

    added in Star Patten Programs

---
+++ Star Pattern/DecreaseTriangle.java
@@ -0,0 +1,10 @@
+public class DecreaseTriangle{
+	public static void main(String[] args){
+		for(int row=1;row<=5;row++){	
+		for(int col=row;col<=5;col++){ //for(int col=5;col>=1;col--)
+			System.out.print("*");
+		}
+	System.out.println();
+	}
+}
+}
--- Star Pattern/DescendingDownwards.java
+++ Star Pattern/DescendingDownwards.java
@@ -10,10 +10,15 @@
 
 public class DescendingDownwards{
 	public static void main(String[] args){
-		for(int i=1;i<=5;i++){
-			for(int j=i+1-1;j<=5;i++){
-			
-			System.out.println();
+		int n=5;
+		for(int i=1;i<=n;i++){				
+			for(int j=1;j<i;j++){
+				System.out.print(" ");
+			}
+			for(int k=5;k>=i;k--){
+				System.out.print("*");
+			}
+		System.out.println();
 		}
 		}
 }
